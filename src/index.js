9b7341916628a733f87b03a3569c8236ae27c59a
dev793 <117709900+dev793@users.noreply.github.com>
2026-01-30 12:34:31 +1300

    Added page switching logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import "./styles.css";
 import { createHomePage } from "./home-page.js";
+import { createMenuPage } from "./menu-page.js";
+import { createAboutPage } from "./about-page.js";
 
 console.log("Hello world!");
+
+const content = document.querySelector("#content");
+const buttons = document.querySelectorAll("button");
+
+const pageCreators = {
+    Home: createHomePage,
+    Menu: createMenuPage,
+    About: createAboutPage,
+}
+
+buttons.forEach(btn => { 
+    btn.addEventListener("click", (event) => {
+        content.innerHTML = "";
+        pageCreators[event.target.textContent]()
+    });
+})
 
 createHomePage();
